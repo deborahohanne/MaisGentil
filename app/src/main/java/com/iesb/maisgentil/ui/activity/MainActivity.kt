06751d86a0edfb32d18acd37d4e1a00a3907e064
C1314412 Deborah Ohanne de Souza Lucena <c1314412@interno.bb.com.br>
2021-04-13 14:28:00 -0300

    Atualização do padrão de projeto

--- app/src/main/java/com/iesb/maisgentil/ui/activity/MainActivity.kt
+++ app/src/main/java/com/iesb/maisgentil/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.iesb.maisgentil
+package com.iesb.maisgentil.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iesb.maisgentil.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
