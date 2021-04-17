857e8b18a1e640ec007e8e895e972f62a5213a69
C1314412 Deborah Ohanne de Souza Lucena <c1314412@interno.bb.com.br>
2021-04-17 15:10:25 -0300

    Implementacao das telas de cadastro

--- app/src/main/java/br/iesb/mobile/gentil/SignUpOneActivity.kt
+++ app/src/main/java/br/iesb/mobile/gentil/SignUpOneActivity.kt
@@ -3,15 +3,25 @@ package br.iesb.mobile.gentil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_sign_up_one.*
 
 class SignUpOneActivity : AppCompatActivity() {
+
+    lateinit var checkVoluntario : CheckBox
+    lateinit var checkRepresentante : CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_one)
 
         btSignUpOne.setOnClickListener {
+            checkVoluntario = this.findViewById(R.id.cbVoluntario);
 
+            if (checkVoluntario.isChecked) {
+                val intencaoDeChamada = Intent(this, SignUpTwoActivity::class.java)
+                startActivity(intencaoDeChamada)
+            }
 
         }
         tvSignUpLoginLinked.setOnClickListener {
@@ -25,4 +35,5 @@ class SignUpOneActivity : AppCompatActivity() {
         }
 
     }
+
 }
