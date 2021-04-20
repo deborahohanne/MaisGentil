61f1c5e4eb959bdb39bfff40418b6a21cb77d52c
C1314412 Deborah Ohanne de Souza Lucena <c1314412@interno.bb.com.br>
2021-04-20 01:03:47 -0300

    Ajuste de botão e código

--- app/src/main/java/br/iesb/mobile/gentil/ui/activity/LoadingActivity.kt
+++ app/src/main/java/br/iesb/mobile/gentil/ui/activity/LoadingActivity.kt
@@ -1,43 +1,22 @@
 package br.iesb.mobile.gentil.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
-import androidx.viewpager2.adapter.FragmentStateAdapter
+
 import br.iesb.mobile.gentil.R
-import br.iesb.mobile.gentil.ui.activity.splash.screen.SplashScreenFragment
+
+import kotlinx.android.synthetic.main.activity_loading.*
 
 class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
 
-        val listaFragmentos = listOf(
-            SplashScreenFragment()
-        )
+        btGentil.setOnClickListener {
+            val intencaoDeChamada = Intent(this, MainActivity::class.java)
+            startActivity(intencaoDeChamada)
 
-        val adaptador = AdaptadorParaConversarComVpLoading(
-            listaFragmentos,
-            supportFragmentManager,
-            lifecycle
-        )
-
-//        vpSplashScreen.adapter = adaptador
-
-
+        }
     }
-
 }
-class AdaptadorParaConversarComVpLoading(
-    val listaFragmentos: List<Fragment>,
-    fragmentManager: FragmentManager,
-    lifecycle: Lifecycle
-) : FragmentStateAdapter(fragmentManager, lifecycle) {
-
-    override fun getItemCount() = listaFragmentos.size
-
-    override fun createFragment(position: Int) = listaFragmentos[position]
-}
-
--- app/src/main/java/br/iesb/mobile/gentil/ui/activity/splash/screen/FragmentSplashscreenBinding.kt
+++
@@ -1,8 +0,0 @@
-package br.iesb.mobile.gentil.ui.activity.splash.screen
-
-import br.iesb.mobile.gentil.ui.activity.splash.screen.SplashScreenFragment
-
-abstract class FragmentSplashscreenBinding {
-
-    abstract var lifecycleOwner: SplashScreenFragment
-}
