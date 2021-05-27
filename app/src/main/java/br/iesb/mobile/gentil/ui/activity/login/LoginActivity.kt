4a967c4b804c68fd0092b9b7549f8f81a7b09682
C1314412 Deborah Ohanne de Souza Lucena <c1314412@interno.bb.com.br>
2021-05-27 19:46:05 -0300

    Injecao de dependencia

--- app/src/main/java/br/iesb/mobile/gentil/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/br/iesb/mobile/gentil/ui/activity/login/LoginActivity.kt
@@ -5,18 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import br.iesb.mobile.gentil.R
+import br.iesb.mobile.gentil.di.LoginModule
 import br.iesb.mobile.gentil.repository.TestClass
 import br.iesb.mobile.gentil.ui.activity.HomeActivity
 import br.iesb.mobile.gentil.ui.activity.MainActivity
 import br.iesb.mobile.gentil.ui.activity.representative.HomeRepresentativeActivity
+import br.iesb.mobile.gentil.ui.activity.voluntary.HomeVoluntaryActivity
 import br.iesb.mobile.gentil.ui.fragment.representative.HomeRepresentantiveFragment
 import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.WithFragmentBindings
 import kotlinx.android.synthetic.main.activity_login.*
+import javax.inject.Inject
 
+//assinatura: algo que pode receber o objeto fornecido
+@AndroidEntryPoint
+@WithFragmentBindings
 class LoginActivity : AppCompatActivity() {
+
+    @Inject lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // utilização do módulo que fabrica o auth
+        /*
+        val fabric = LoginModule()
+        val auth = fabric.firebaseAuthProvider()
+        */
 
         tvLoginTwo.setOnClickListener {
             val callIntent = Intent(this, ForgotActivity::class.java)
@@ -27,8 +44,8 @@ class LoginActivity : AppCompatActivity() {
             val email = etEmailAddressLogin.text.toString()
             val password = etPasswordLogin.text.toString()
 
-           /* recuperar uma instância do firebase o objeto de autenticação */
-            val auth = FirebaseAuth.getInstance()
+            /* *//* recuperar uma instância do firebase o objeto de autenticação *//*
+            val auth = FirebaseAuth.getInstance()*/
 
             /* função, dentro do obj 'auth', método que recebe o email e senha para efetuar o login
             retorna promessa de resultado */
@@ -37,9 +54,7 @@ class LoginActivity : AppCompatActivity() {
             /* verificar se foi efetuado o login com sucesso */
             taskDeLogin.addOnCompleteListener{ resultado ->
                 if (resultado.isSuccessful) {
-                    val intentGoToHomeScreen = Intent(this, HomeRepresentativeActivity::class.java)
-                    TestClass().setValue()
-                    TestClass().getValue()
+                    val intentGoToHomeScreen = Intent(this, HomeVoluntaryActivity::class.java)
                     startActivity((intentGoToHomeScreen))
                     // Toast.makeText(this, "Olha, deu certo o Login", Toast.LENGTH_LONG).show()
                 } else {
