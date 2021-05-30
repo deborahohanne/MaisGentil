b5b25517599fc325beb224e9f7c19c65de24adb3
C1314412 Deborah Ohanne de Souza Lucena <c1314412@interno.bb.com.br>
2021-05-30 16:30:32 -0300

    atualizacoes de teste

---
+++ app/src/main/java/br/iesb/mobile/gentil/ui/activity/MainStartActivity.kt
@@ -0,0 +1,11 @@
+package br.iesb.mobile.gentil.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_start)
+    }
+}
