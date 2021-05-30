adbce59fccc417e517ef6ecf4b56aef2d73c5dad
Amanda Elisa <amandaelisa47@gmail.com>
2021-05-30 16:23:11 -0300

    Bottom bar

--- app/src/main/java/br/iesb/mobile/gentil/ui/activity/HomeActivity.kt
+++ app/src/main/java/br/iesb/mobile/gentil/ui/activity/HomeActivity.kt
@@ -3,10 +3,13 @@ package br.iesb.mobile.gentil.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.iesb.mobile.gentil.R
+import com.google.android.material.navigation.NavigationBarView
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottomNavigationView.background = null
     }
 }
