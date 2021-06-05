b82f47d4cc525734137ec2da7a4fb0b3f292e7ef
Deborah Ohanne <deborahohanne@ohanne.local>
2021-06-05 04:27:52 -0300

    FIX: parte da navegação main e fragmento início da implementação do fragment register

--- app/src/main/java/br/iesb/mobile/gentil/ui/fragment/login/RegisterFragment.kt
+++ app/src/main/java/br/iesb/mobile/gentil/ui/fragment/login/RegisterFragment.kt
@@ -1,18 +1,33 @@
 package br.iesb.mobile.gentil.ui.fragment.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import br.iesb.mobile.gentil.R
 import br.iesb.mobile.gentil.databinding.FragmentLoginBinding
 import br.iesb.mobile.gentil.databinding.FragmentRegisterBinding
+import br.iesb.mobile.gentil.ui.activity.representative.RegisterRepresentativeActivity
+import br.iesb.mobile.gentil.ui.activity.voluntary.RegisterVoluntaryActivity
+import kotlinx.android.synthetic.main.activity_register.*
 
 
 class RegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
+
+    /* definições de variáveis referente ao 'radio group' da tela*/
+    var radioGroup: RadioGroup? = null
+    private lateinit var radioButton: RadioButton
+    lateinit var button: Button
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +40,27 @@ class RegisterFragment : Fragment() {
         // o código define o ciclo de vida do fragmento
         binding.lifecycleOwner = this
 
+        radioGroup = binding.rgSignupOne
+        button = binding.btSignUpOne
+
+        button.setOnClickListener { buttonCheck() }
+
         return binding.root
 
     }
 
+    private fun buttonCheck() {
+        val intSelectButton: Int = radioGroup!!.checkedRadioButtonId
+
+        if (intSelectButton.equals(rbCheckVoluntario)) {
+            findNavController().navigate(R.id.action_registerFragment_to_registerVoluntaryFragment)
+        }
+
+        if (intSelectButton.equals(rbCheckRepresentante)) {
+            findNavController().navigate(R.id.action_registerFragment_to_registerRepresentativeFragment)
+        }
+    }
+
 
 }
+
---
+++ app/src/main/java/br/iesb/mobile/gentil/ui/fragment/voluntary/EditProfileVoluntaryFragment.kt
@@ -0,0 +1,25 @@
+package br.iesb.mobile.gentil.ui.fragment.voluntary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.iesb.mobile.gentil.R
+
+class EditProfileVoluntaryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile_voluntary, container, false)
+    }
+
+}
