55fc477e7ed739a774178d02d7a55e4a950acade
Amanda Elisa <amandaelisa47@gmail.com>
2021-06-15 20:09:23 -0300

    test

--- app/src/main/java/br/iesb/mobile/gentil/ui/activity/voluntary/CreateAdVoluntaryActivity.kt
+++ app/src/main/java/br/iesb/mobile/gentil/ui/activity/voluntary/CreateAdVoluntaryActivity.kt
@@ -1,12 +1,16 @@
 package br.iesb.mobile.gentil.ui.activity.voluntary
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.iesb.mobile.gentil.R
+import kotlinx.android.synthetic.main.activity_create_ad_voluntary.*
 
 class CreateAdVoluntaryActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_ad_voluntary)
+
     }
 }
