3dd9d6685675c540277394071c10e4b530bc9178
Deborah Ohanne <deborahohanne@ohanne.local>
2021-06-16 01:52:50 -0300

    FIX: Ajustes e implementacao de forgot e register

--- app/src/main/java/br/iesb/mobile/gentil/ui/fragment/login/RegisterVoluntaryFragment.kt
+++ app/src/main/java/br/iesb/mobile/gentil/ui/fragment/login/RegisterVoluntaryFragment.kt
@@ -5,56 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.iesb.mobile.gentil.R
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import br.iesb.mobile.gentil.databinding.FragmentRegisterVoluntaryBinding
+import br.iesb.mobile.gentil.viewmodel.LoginViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterVoluntaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterVoluntaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentRegisterVoluntaryBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val viewModel: LoginViewModel by lazy {
+        ViewModelProvider(this).get(LoginViewModel::class.java)
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_voluntary, container, false)
+
+        binding = FragmentRegisterVoluntaryBinding.inflate(inflater, container, false)
+
+        // o código é a própria classe
+        binding.codigoFragmento = this
+        // o código define o ciclo de vida do fragmento
+        binding.lifecycleOwner = this
+
+        binding.btSignUpVoluntario.setOnClickListener{ registerVoluntary() }
+
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterVoluntaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterVoluntaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun registerVoluntary(){
+        val email = binding.etEmailSignUpVoluntario.text.toString()
+        val password = binding.etPasswordSignUpVoluntario.text.toString()
+        val confirmPassword = binding.etConfirmPasswordSignUpVoluntario.text.toString()
+
+        viewModel.registerUser(email, password, confirmPassword) { response ->
+            Toast.makeText(this.context, response[1], Toast.LENGTH_LONG).show()
+            if (response[0] == "OK") {
+                Toast.makeText(this.context, response[1], Toast.LENGTH_LONG).show()
             }
+        }
+
     }
+
 }
